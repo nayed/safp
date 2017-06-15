6366a29823a40740562a1bed2bc5110a5262a63c
Nayed Saïd Ali <nayed.saidali@gmail.com>
2017-06-15 15:26:53 +0200

    search and print occurence

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate safp;
+
+use std::env;
+use std::process;
+
+use safp::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("☹️ Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.filename);
+
+    if let Err(e) = safp::run(config) {
+        println!("🔥 Application error: {}", e);
+        process::exit(1);
+    }
 }
